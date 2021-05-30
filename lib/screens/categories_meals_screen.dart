c9a23a30e54acd20a35087b56be817117480ada8
IsmaelCardoso <ismael.cardosos@gmail.com>
2021-05-30 15:55:16 -0300

    chore(categories_meals_screen and category_item): navigating to a screen

---
+++ lib/screens/categories_meals_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recipes'),
+      ),
+      body: Center(child: Text('Recipes for Categories')),
+    );
+  }
+}
