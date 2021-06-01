e48d0bc59b611ec6008790594e760f4af00dda37
IsmaelCardoso <ismael.cardosos@gmail.com>
2021-06-01 12:56:58 -0300

    chore(meal_detail): added TabBar to AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/categories_meals_screen.dart';
 import 'screens/categories.screen.dart';
 import 'screens/meal_detail_screen.dart';
+import 'screens/tabs_screen.dart';
 import './utils/app_routes.dart';
 
 main() => runApp(MyApp());
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
             ),
       ),
       routes: {
-        AppRoutes.HOME: (context) => CategoriesScreen(),
+        AppRoutes.HOME: (context) => TabScreen(),
         AppRoutes.CATEGORIES_MEALS: (context) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (context) => MealDetailScreen(),
       },
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../screens/categories.screen.dart';
+import '../screens/favorite_screen.dart';
+
+class TabScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Let's cook"),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.category),
+                text: 'Categories',
+              ),
+              Tab(
+                icon: Icon(Icons.star),
+                text: 'Favorites',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            CategoriesScreen(),
+            FavoriteScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
